070c5fc87b83cdf38500d9b6ec90b1cb585a8fac
Sergey Berezin <sergey@sergeyberezin.com>
2017-07-06 13:15:30 -0700

    Made setup.py work. (#3)
    Had to work around a bug (?) in setup.py where it tried to compile
    `cryptography` package, while `pip install` would simply install a
    precompiled wheel.

--- setup.py
+++ setup.py
@@ -13,7 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from distutils.command.install import install
 from setuptools import setup, find_packages
+import subprocess
+
+INSTALL_REQUIRES = [
+    'acme-tiny',
+    'cryptography',
+    'protobuf',
+]
+
+
+def pip_install(package):
+    p = subprocess.Popen(['pip', 'install', package], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.split('\n'):
+        print(line)
+
+
+# For some reason, on Mac OS X `./setup.py install` wants to build
+# cryptography package, while `pip install cryptography` just installs
+# a prebuilt wheel. Here we force `pip install` for all the
+# requirements before attempting to install through setup.py.
+class Install(install):
+  def run(self):
+    for req in INSTALL_REQUIRES:
+      pip_install(req)
+    install.run(self)
+
 
 setup(
     name='acme-tiny-cron',
@@ -23,12 +50,11 @@ setup(
     classifiers=[
         'Programming Language :: Python',
     ],
+    cmdclass = {
+      'install': Install,
+    },
     packages=find_packages(exclude=['*.test']),
-    install_requires=[
-        'acme-tiny',
-        'cryptography',
-        'protobuf',
-    ],
+    install_requires=INSTALL_REQUIRES,
     package_data={
         '': ['*.md'],
     },
